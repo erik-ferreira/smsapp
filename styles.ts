404037881a658d4db7271f77f5290771723de87f
erik-ferreira <erikdesouzagf@gmail.com>
2023-09-26 15:47:17 -0300

    create structure

---
+++ styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121214",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 16,
+    padding: 32,
+  },
+
+  input: {
+    width: 64,
+    height: 64,
+    backgroundColor: "#8257E5",
+    color: "#FFF",
+    borderRadius: 8,
+
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+})
